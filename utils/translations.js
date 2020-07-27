845d69ec91032a4f2e1d86a777ea72787bce616d
Ricardo Lüders <ricardo@luders.com.br>
2020-07-27 11:23:04 +0200

    Remove python script

---
+++ utils/translations.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
+
+const SRC_DIR = "./src";
+const LANG_DIR = "./public/lang";
+
+let languages = {};
+
+const searchLanguageFiles = () => {
+  glob.sync(`${SRC_DIR}/**/translations/**/*.json`).map((globFilePath) => {
+    const paths = globFilePath.split("/").reverse();
+
+    const file = paths[0].toLowerCase().replace(".json", "");
+    const lang = paths[1].toLowerCase();
+    const module = paths[3] == "src" ? "__DEFAULT__" : paths[3].toLowerCase();
+
+    const data = fs.readFileSync(globFilePath, "utf8");
+
+    const jsonData = JSON.parse(data);
+    if (jsonData === {}) {
+      return;
+    }
+
+    if (languages[lang] === undefined) {
+      const obj = {};
+      obj[module] = {};
+      obj[module][file] = {};
+      languages[lang] = obj;
+    }
+
+    if (languages[lang][module] == undefined) {
+      languages[lang][module] = {};
+    }
+
+    const obj = {};
+    obj[file] = { ...jsonData };
+
+    languages[lang][module] = {
+      ...languages[lang][module],
+      ...obj,
+    };
+  });
+};
+
+const generateFiles = () => {
+  Object.keys(languages).map((lang) => {
+    const langPath = path.join(LANG_DIR, lang);
+
+    if (!fs.existsSync(langPath)) {
+      fs.mkdirSync(langPath);
+    }
+
+    Object.keys(languages[lang]).map((module) => {
+      const modulePath =
+        module == "__DEFAULT__"
+          ? path.join(langPath, "translation.json")
+          : path.join(langPath, `${module}.json`);
+
+      const data = JSON.stringify(languages[lang][module]);
+
+      fs.writeFileSync(modulePath, data, {
+        encoding: "utf8",
+        flag: "w+",
+      });
+    });
+  });
+};
+
+try {
+  searchLanguageFiles();
+
+  if (languages) {
+    generateFiles();
+    console.log("✔ All translation files successfully generated.");
+  } else {
+    console.log("❌ Unable to generate translation files.");
+  }
+} catch (err) {
+  console.log("⚠ Something went terrible wrong.", err);
+}
