b5483d187097f02676c5e921db229956e9f5625b
Camila Moura <camilapaleo@gmail.com>
2021-10-13 16:39:02 +0200

    fix: clean code

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,10 @@ import './services/i18n';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { useTheme } from './contexts/ThemeProvider';
 import AuthProvider from './modules/Auth/contexts/auth';
 import Routes from './routes';
 
 const App: React.FC = () => {
-  const { theme } = useTheme();
-  console.log(theme);
-
   return (
     <div className="flex flex-row min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white">
       <BrowserRouter>
