577bb87d8d97e777f94f81cda87529499d660499
Ricardo Lüders <rluders@redhat.com>
2021-10-26 18:30:26 +0200

    feat: add missing course route props

--- src/modules/Course/containers/CourseOverview/index.tsx
+++ src/modules/Course/containers/CourseOverview/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+type RouteParams = {
+  course: string;
+};
 
 const CourseOverview: React.FC = () => {
-  return <>THIS IS THE COURSE OVERVIEW PAGE</>;
+  const { course } = useParams<RouteParams>();
+  return <>THIS IS THE COURSE {course} OVERVIEW PAGE</>;
 };
 
 export default CourseOverview;
--- src/modules/Course/containers/CourseProjects/index.tsx
+++ src/modules/Course/containers/CourseProjects/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+type RouteParams = {
+  course: string;
+};
 
 const CourseProjects: React.FC = () => {
-  return <>THIS IS THE COURSE PROJECTS PAGE</>;
+  const { course } = useParams<RouteParams>();
+
+  return <>THIS IS THE COURSE {course} PROJECTS PAGE</>;
 };
 
 export default CourseProjects;
