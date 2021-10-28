0795efe09d16f1902e171daed0ce9433528f20c3
Camila Moura <camilapaleo@gmail.com>
2021-10-28 18:16:27 +0200

    wip: feat: sidebar

--- src/layouts/Default/Sidebar/index.tsx
+++ src/layouts/Default/Sidebar/index.tsx
@@ -1,31 +1,78 @@
 import Logo from '@app/components/Logo';
+import {
+  ChatIcon,
+  CogIcon,
+  FolderOpenIcon,
+  HomeIcon,
+  QuestionMarkCircleIcon,
+} from '@heroicons/react/solid';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
+  const mainMenuItems = [
+    { route: '/', icon: HomeIcon },
+    { route: '/', icon: FolderOpenIcon },
+    { route: '/', icon: ChatIcon },
+  ];
+
+  const settingsMenuItems = [
+    { route: '/', icon: QuestionMarkCircleIcon },
+    { route: '/', icon: CogIcon },
+  ];
+
   return (
     <aside
-      className="sidebar w-16 transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-gray-900"
+      className="sidebar fixed flex flex-col h-screen justify-center w-24 m-0 
+      bg-primary justify-between divide divide-y"
       data-testid="sidebar-element"
     >
-      <div className="sidebar-header flex items-center justify-center bg-white py-4">
-        <div className="flex h-8 dark:text-primary">
-          <Logo />
+      <div>
+        <div className="sidebar-header items-center justify-center bg-white py-4">
+          <div className="flex h-8 dark:text-primary">
+            <Logo />
+          </div>
+        </div>
+        <div>
+          <MenuList items={mainMenuItems} />
         </div>
       </div>
-      <div className="sidebar-content px-4 py-6">
-        <ul className="flex flex-col w-full">
-          <li className="my-px">
-            <a
-              href=""
-              className="flex flex-row items-center h-10 px-2 rounded-lg bg-gray-100"
-            >
-              Icon
-            </a>
-          </li>
-        </ul>
+      <div>
+        <MenuList items={settingsMenuItems} />
       </div>
     </aside>
   );
 };
+
+interface MenuListProps {
+  items: MenuListItemProps[];
+}
+
+const MenuList = ({ items }: MenuListProps) => (
+  <ul className="flex flex-col w-full">
+    {items.map((item) => (
+      <MenuListItem key={item.route} route={item.route} icon={item.icon} />
+    ))}
+  </ul>
+);
+
+interface MenuListItemProps {
+  route: string;
+  icon: React.ComponentType<any>;
+}
+
+const MenuListItem = ({ route, icon: IconComponent }: MenuListItemProps) => (
+  <li>
+    <Link
+      to={route}
+      className="flex-row items-center justify-center h-12 w-12 px-2 mt-2 mb-2 
+    rounded-lg group hover:bg-gray-100 hover:shadow-lg transition-transform duration-150 "
+    >
+      <div className="text-white group-hover:text-red-600 h-6 w-6 m-auto ">
+        <IconComponent />
+      </div>
+    </Link>
+  </li>
+);
 
 export default Sidebar;
