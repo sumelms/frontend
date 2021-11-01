47e28545231fcfc33582dbd0c549e9da58f36b68
Camila Moura <41678283+mouracamila@users.noreply.github.com>
2021-11-01 17:27:06 +0100

    Create sidebar (#67)
    * wip: feat: sidebar
    
    * wip: feat: sidebar
    
    * fix: code review
    
    * chore: unit tests to sidebar
    
    Co-authored-by: Ricardo Lüders <rluders@redhat.com>
    Co-authored-by: Ricardo Lüders <ricardo@luders.com.br>

--- src/layouts/Default/Sidebar/index.tsx
+++ src/layouts/Default/Sidebar/index.tsx
@@ -1,31 +1,76 @@
 import Logo from '@app/components/Logo';
+import {
+  ChatIcon,
+  CogIcon,
+  FolderOpenIcon,
+  HomeIcon,
+  QuestionMarkCircleIcon,
+} from '@heroicons/react/solid';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
+  const mainMenuItems = [
+    { route: '/', icon: HomeIcon },
+    { route: '/', icon: FolderOpenIcon },
+    { route: '/', icon: ChatIcon },
+  ];
+
+  const settingsMenuItems = [
+    { route: '/', icon: QuestionMarkCircleIcon },
+    { route: '/', icon: CogIcon },
+  ];
+
   return (
     <aside
-      className="sidebar w-16 transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-gray-900"
+      className="fixed flex flex-col justify-between w-20 h-screen m-0 divide-y sidebar bg-primary divide"
       data-testid="sidebar-element"
     >
-      <div className="sidebar-header flex items-center justify-center bg-white py-4">
-        <div className="flex h-8 dark:text-primary">
-          <Logo />
+      <div>
+        <div className="items-center justify-center py-4 bg-white sidebar-header">
+          <div className="flex h-8 dark:text-primary">
+            <Logo />
+          </div>
+        </div>
+        <div className="mt-2">
+          <MenuList items={mainMenuItems} />
         </div>
       </div>
-      <div className="sidebar-content px-4 py-6">
-        <ul className="flex flex-col w-full">
-          <li className="my-px">
-            <a
-              href=""
-              className="flex flex-row items-center h-10 px-2 rounded-lg bg-gray-100"
-            >
-              Icon
-            </a>
-          </li>
-        </ul>
+      <div>
+        <MenuList items={settingsMenuItems} />
       </div>
     </aside>
   );
 };
+
+interface MenuListProps {
+  items: MenuListItemProps[];
+}
+
+const MenuList = ({ items }: MenuListProps) => (
+  <ul className="flex flex-col w-full">
+    {items.map((item, key) => (
+      <MenuListItem key={key} route={item.route} icon={item.icon} />
+    ))}
+  </ul>
+);
+
+interface MenuListItemProps {
+  route: string;
+  icon: React.ComponentType<any>;
+}
+
+const MenuListItem = ({ route, icon: IconComponent }: MenuListItemProps) => (
+  <li className="flex justify-center ">
+    <Link
+      to={route}
+      className="flex flex-row items-center justify-center w-12 h-12 mt-2 mb-2 transition-transform duration-150 rounded-lg group hover:bg-gray-100 hover:shadow-lg "
+    >
+      <div className="w-6 h-6 m-auto text-white group-hover:text-red-600 ">
+        <IconComponent />
+      </div>
+    </Link>
+  </li>
+);
 
 export default Sidebar;
--- src/layouts/Default/Sidebar/tests.spec.tsx
+++ src/layouts/Default/Sidebar/tests.spec.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
+import { cleanup, render } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Sidebar from './index';
 
 describe('Sidebar component', () => {
   afterEach(cleanup);
 
   it('should be able to render an sidebar', () => {
-    const { getByTestId } = render(<Sidebar />);
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>,
+    );
     expect(getByTestId('sidebar-element')).toBeTruthy();
   });
 
   it('should render correctly', () => {
-    const { getByTestId } = render(<Sidebar />);
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>,
+    );
     expect(getByTestId('sidebar-element')).toHaveClass('sidebar');
   });
 });
