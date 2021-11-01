167ec6ecd55d20345719938fcfd6be3cf09118c5
Ricardo Lüders <rluders@redhat.com>
2021-11-01 20:38:52 +0100

    chore: keycloack mock for jest

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,18 +1,6 @@
-import React from 'react';
-
 import { render } from '@testing-library/react';
+import React from 'react';
 import App from './App';
-
-let mockInitialized = false;
-let mockKeycloakStub = 'mock-stub';
-
-jest.mock('@react-keycloak/web', () => {
-  const originalModule = jest.requireActual('@react-keycloak/web');
-  return {
-    ...originalModule,
-    useKeycloak: () => [mockKeycloakStub, mockInitialized],
-  };
-});
 
 describe('<App> component', () => {
   it('renders without crashing', () => {
