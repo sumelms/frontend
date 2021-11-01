5529fab5c42b837a71105d2cf8096e1dc68f139b
Ricardo Lüders <rluders@redhat.com>
2021-11-01 20:37:25 +0100

    chore: removing unecessary comments

--- craco.config.js
+++ craco.config.js
@@ -9,11 +9,7 @@ module.exports = {
       plugin: require('craco-alias'),
       options: {
         source: 'tsconfig',
-        // baseUrl SHOULD be specified
-        // plugin does not take it from tsconfig
         baseUrl: './src',
-        /* tsConfigPath should point to the file where "baseUrl" and "paths" 
-          are specified*/
         tsConfigPath: './tsconfig.paths.json',
       },
     },
