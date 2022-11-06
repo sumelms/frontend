5529c7934b3d613aecdd6d2f9abc1b0a6f2fa300
Camila <41678283+mouracamila@users.noreply.github.com>
2022-11-06 15:05:37 +0100

    feat: adds CardEducator component (#106)
    * feat: adds CardEducator component
    
    * add space between cards
    
    * add space between title and card content

--- src/modules/Course/containers/CourseOverview/index.tsx
+++ src/modules/Course/containers/CourseOverview/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import SectionFaculty from './SectionFaculty';
+
 type RouteParams = {
   course: string;
 };
 
 const CourseOverview: React.FC = () => {
   const { course } = useParams<RouteParams>();
-  return <>THIS IS THE COURSE {course} OVERVIEW PAGE</>;
+  return (
+    <>
+      THIS IS THE COURSE {course} OVERVIEW PAGE
+      <SectionFaculty />
+    </>
+  );
 };
 
 export default CourseOverview;
