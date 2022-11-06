b99366fcce9cc6af706c6230d9805798277a8483
Ricardo Lüders <rluders@redhat.com>
2022-11-06 15:12:31 +0100

    feat(course): course page navigation (#115)

---
+++ src/modules/Course/components/PageNavbar/index.tsx
@@ -0,0 +1,62 @@
+import { Breadcrumb, Tabs } from 'flowbite-react';
+import React from 'react';
+import { HiHome } from 'react-icons/hi';
+
+// interface PageHeaderProps {}
+
+interface PageHeaderTheme {
+  navbar: {
+    base: string;
+    item: {
+      base: string;
+    };
+  };
+}
+
+const PageHeader: React.FC = () => {
+  const theme: PageHeaderTheme = {
+    navbar: {
+      base: 'flex gap-2',
+      item: {
+        base: 'flex hover:bg-red-100 hover:text-red-600 rounded-lg items-center justify-center p-3 text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500',
+      },
+    },
+  };
+
+  return (
+    <div className="my-8 space-y-8">
+      <div>
+        <Breadcrumb aria-label="Default breadcrumb example">
+          <Breadcrumb.Item href="/" icon={HiHome}>
+            Home
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+      <div className={theme.navbar.base}>
+        <a className={theme.navbar.item.base} href="/">
+          Apresentação
+        </a>
+        <a className={theme.navbar.item.base} href="/">
+          Matriz Curricular
+        </a>
+        <a className={theme.navbar.item.base} href="/">
+          Instrição nas turmas
+        </a>
+        <a className={theme.navbar.item.base} href="/">
+          Minhas turmas
+        </a>
+        <a className={theme.navbar.item.base} href="/">
+          Projetos
+        </a>
+        <a className={theme.navbar.item.base} href="/">
+          Fórum de discussão
+        </a>
+        <a className={theme.navbar.item.base} href="/">
+          Alunos do curso
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default PageHeader;
