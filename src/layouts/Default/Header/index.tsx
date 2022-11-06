1f9199f53cbff16d272e6af7e524ebe7e15677c9
Ricardo Lüders <rluders@redhat.com>
2022-11-06 14:51:05 +0100

    feat(navbar): user dropdown

--- src/layouts/Default/Header/index.tsx
+++ src/layouts/Default/Header/index.tsx
@@ -1,6 +1,7 @@
 import { useKeycloak } from '@react-keycloak/web';
-import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
 import React from 'react';
+import { HiLogout } from 'react-icons/hi';
 
 const Header: React.FC = () => {
   const { keycloak } = useKeycloak();
@@ -11,13 +12,34 @@ const Header: React.FC = () => {
     avatar: 'https://avatars.githubusercontent.com/u/191027?v=4',
   };
 
+  const ActionButtons = () => {
+    return (
+      <div className="pl-4 ml-4 border-l">
+        <Button
+          onClick={() => keycloak.logout()}
+          className="!bg-transparent !text-gray-600"
+        >
+          <HiLogout className="w-4 h-4" />
+        </Button>
+      </div>
+    );
+  };
+
   const UserDetails = () => {
     return (
-      <Avatar
-        alt="User settings"
-        img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-        rounded={true}
-      />
+      <div className="flex items-center">
+        <div className="flex flex-col mr-2 text-right">
+          <p className="text-sm font-semibold">{user.name}</p>
+          <span className="text-xs font-medium">{user.role}</span>
+        </div>
+        <div>
+          <Avatar
+            alt={`${user.name}'s settings menu`}
+            img={user.avatar}
+            rounded={true}
+          />
+        </div>
+      </div>
     );
   };
 
@@ -31,7 +53,7 @@ const Header: React.FC = () => {
         />
       </Navbar.Brand>
       <div className="flex bg-white md:order-2">
-        <Dropdown arrowIcon={false} inline={true} label={<UserDetails />}>
+        <Dropdown arrowIcon={true} inline={true} label={<UserDetails />}>
           <Dropdown.Header>
             <span className="block text-sm">Bonnie Green</span>
             <span className="block text-sm font-medium truncate">
@@ -46,6 +68,7 @@ const Header: React.FC = () => {
             Sign out
           </Dropdown.Item>
         </Dropdown>
+        <ActionButtons />
       </div>
     </Navbar>
   );
