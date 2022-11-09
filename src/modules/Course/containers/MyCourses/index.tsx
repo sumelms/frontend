1303627146efa93c3d08af358998858b138fa87f
Ricardo Lüders <rluders@redhat.com>
2022-11-09 17:30:56 +0100

    feat(course): real course routes

--- src/modules/Course/containers/MyCourses/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyCourses: React.FC = () => {
-  return <>THIS IS THE MY COURSES PAGE</>;
-};
-
-export default MyCourses;
--- src/modules/Course/routes/index.tsx
+++ src/modules/Course/routes/index.tsx
@@ -1,60 +1,36 @@
 import DefaultLayout from '../../../layouts/Default';
 import { RouteProps as Route } from '../../../router/RouteWrapper';
-import CourseActivity from '../containers/CourseActivity';
-import CourseCommunity from '../containers/CourseCommunity';
-import CourseContents from '../containers/CourseContents';
+import CourseClassrooms from '../containers/CourseClassrooms';
+import CourseClassroomsSubscription from '../containers/CourseClassroomsSubscription';
+import CourseMatrix from '../containers/CourseMatrix';
 import CourseOverview from '../containers/CourseOverview';
-import CourseProjects from '../containers/CourseProjects';
-import CourseStudents from '../containers/CourseStudents';
-import MyCourses from '../containers/MyCourses';
 
 const routes: Route[] = [
   {
-    path: '/courses/my',
-    name: 'My Courses',
-    component: MyCourses,
-    layout: DefaultLayout,
-    exact: true,
-  },
-  {
-    path: '/courses/:course',
+    path: '/course/:course',
     name: 'Course Overview',
     component: CourseOverview,
     layout: DefaultLayout,
     exact: true,
   },
   {
-    path: '/courses/:course/contents',
-    name: 'Course Content',
-    component: CourseContents,
+    path: '/course/:course/classrooms',
+    name: 'Course Classrooms',
+    component: CourseClassrooms,
     layout: DefaultLayout,
     exact: true,
   },
   {
-    path: '/courses/:course/community',
-    name: 'Course Community',
-    component: CourseCommunity,
+    path: '/course/:course/classrooms/subscription',
+    name: 'Course Classrooms Subscription',
+    component: CourseClassroomsSubscription,
     layout: DefaultLayout,
     exact: true,
   },
   {
-    path: '/courses/:course/projects',
-    name: 'Course Projects',
-    component: CourseProjects,
-    layout: DefaultLayout,
-    exact: true,
-  },
-  {
-    path: '/courses/:course/students',
-    name: 'Course Students',
-    component: CourseStudents,
-    layout: DefaultLayout,
-    exact: true,
-  },
-  {
-    path: '/courses/:course/activity/:activity',
-    name: 'Course Activity',
-    component: CourseActivity,
+    path: '/course/:course/matrix',
+    name: 'Course Matrix',
+    component: CourseMatrix,
     layout: DefaultLayout,
     exact: true,
   },
