cc1c013eae6d2e3e5491ef5691fa09bdc3e4d275
mouracamila <camilapaleo@gmail.com>
2022-11-20 20:03:21 +0100

    this change  the CardEducator component style with:  spaces between elements, font-size and replace link to button

--- src/modules/Course/components/CardEducator/index.tsx
+++ src/modules/Course/components/CardEducator/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card } from 'flowbite-react';
+import { Avatar, Button, Card } from 'flowbite-react';
 import React from 'react';
 import { IconType } from 'react-icons/lib';
 
@@ -31,39 +31,24 @@ const CardEducator: React.FC<CardEducatorProps> = ({
   action,
 }) => {
   return (
-    <Card className="lg:w-72">
-      <div className="flex flex-col my-2">
-        <div className="self-center justify-center space-y-3">
+    <Card className="max-w-xs">
+      <div className="flex flex-col space-y-6">
+        <div className="self-center justify-center space-y-2 ">
           <Avatar img={educator.avatar} size="xl" rounded={true} />
-          <p className="text-gray-900 text-center	dark:text-white">
+
+          <p className="font-bold text-center text-gray-900 dark:text-white">
             {educator.name}
           </p>
-          <p className="font-bold text-center text-gray-900 dark:text-white">
+          <p className="text-sm font-bold text-center text-gray-600 dark:text-white">
             {educator.title}
           </p>
-        </div>
-
-        <div className="my-6">
-          {action.map(({ icon: Icon, name }, key) => (
-            <div key={key} className="h-12 flex">
-              <a
-                href="/"
-                className="flex items-center gap-x-5 hover:text-blue-500 hover:delay-150"
-              >
-                <Icon className="w-5 h-5" />
-                <p className="text-sm font-semibold text-gray-600 dark:text-white hover:text-blue-500 hover:underline hover:underline-offset-4">
-                  {name}
-                </p>
-              </a>
-            </div>
-          ))}
         </div>
 
         <div className="space-y-2">
           {info.map(({ icon: Icon, name, value }, key) => (
             <div key={key} className="flex">
-              <Icon className="h-4	text-red-700" />
-              <div className="flex w-full justify-between ml-4">
+              <Icon className="h-4 text-red-700" />
+              <div className="flex justify-between w-full ml-4">
                 <p className="text-xs font-semibold text-gray-600 dark:text-white">
                   {name}
                 </p>
@@ -72,6 +57,15 @@ const CardEducator: React.FC<CardEducatorProps> = ({
                 </p>
               </div>
             </div>
+          ))}
+        </div>
+
+        <div className="space-y-2">
+          {action.map(({ icon: Icon, name }, key) => (
+            <Button key={key} fullSized color="light">
+              <Icon className="w-5 h-5 mr-3" />
+              <p className="text-sm font-semibold dark:text-white ">{name}</p>
+            </Button>
           ))}
         </div>
       </div>
