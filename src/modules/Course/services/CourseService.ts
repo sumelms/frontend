1cd42daedb54a9debf7f5216de37c6d9aa5e980e
mouracamila <camilapaleo@gmail.com>
2022-11-20 20:00:00 +0100

    add more text data and more educators in fake-api CourseService

--- src/modules/Course/services/CourseService.ts
+++ src/modules/Course/services/CourseService.ts
@@ -41,7 +41,8 @@ const CourseService = {
   getCourseOverview(uid: string) {
     return {
       description:
-        'A área de tecnologia da informação está em constante crescimento, sempre com oportunidades para os profissionais do ramo. O curso de Sistemas de Informação da USP proporciona aos alunos uma formação sólida de desenvolvimento de software, com toda a estrutura necessária para o ensino. Como é uma área muito dinâmica, o curso também realiza palestras, cursos e demais eventos ao longo da graduação para manter os alunos atualizados com as novidades do mercado. A região de Ribeirão Preto é privilegiada porque abriga muitas empresas do ramo de TI, mas o profissional também pode atuar em praticamente todos os setores, oferecendo suporte e criando inovações digitais.',
+        'A área de tecnologia da informação está em constante crescimento, sempre com oportunidades para os profissionais do ramo. O curso de Sistemas de Informação da USP proporciona aos alunos uma formação sólida de desenvolvimento de software, com toda a estrutura necessária para o ensino. Como é uma área muito dinâmica, o curso também realiza palestras, cursos e demais eventos ao longo da graduação para manter os alunos atualizados com as novidades do mercado. A área de tecnologia da informação está em constante crescimento, sempre com oportunidades para os profissionais do ramo. O curso de Sistemas de Informação da USP proporciona aos alunos uma formação sólida de desenvolvimento de software, com toda a estrutura necessária para o ensino. Como é uma área muito dinâmica, o curso também realiza palestras, cursos e demais eventos ao longo da graduação para manter os alunos atualizados com as novidades do mercado. A região de Ribeirão Preto é privilegiada porque abriga muitas empresas do ramo de TI, mas o profissional também pode atuar em praticamente todos os setores, oferecendo suporte e criando inovações digitais. A região de Ribeirão Preto é privilegiada porque abriga muitas empresas do ramo de TI, mas o profissional também pode atuar em praticamente todos os setores, oferecendo suporte e criando inovações digitais.',
+
       gallery: [
         {
           img: 'https://placeimg.com/640/480/tech',
@@ -60,13 +61,43 @@ const CourseService = {
       educators: [
         {
           avatar:
-            'https://flowbite.com/docs/images/people/profile-picture-5.jpg',
+            'https://flowbite.com/docs/images/people/profile-picture-1.jpg',
+          name: 'Prof. Dr. Jack Crawford',
+          title: 'Vice-Coordenador',
+        },
+        {
+          avatar:
+            'https://flowbite.com/docs/images/people/profile-picture-2.jpg',
+          name: 'Prof. Dr. Jack Crawford',
+          title: 'Vice-Coordenador',
+        },
+        {
+          avatar:
+            'https://flowbite.com/docs/images/people/profile-picture-3.jpg',
+          name: 'Prof. Dr. Jack Crawford',
+          title: 'Vice-Coordenador',
+        },
+        {
+          avatar:
+            'https://flowbite.com/docs/images/people/profile-picture-4.jpg',
           name: 'Prof. Dr. Jack Crawford',
           title: 'Vice-Coordenador',
         },
         {
           avatar:
             'https://flowbite.com/docs/images/people/profile-picture-5.jpg',
+          name: 'Prof. Dr. Jack Crawford',
+          title: 'Vice-Coordenador',
+        },
+        {
+          avatar:
+            'https://flowbite.com/docs/images/people/profile-picture-3.jpg',
+          name: 'Prof. Dr. Jack Crawford',
+          title: 'Vice-Coordenador',
+        },
+        {
+          avatar:
+            'https://flowbite.com/docs/images/people/profile-picture-4.jpg',
           name: 'Prof. Dr. Jack Crawford',
           title: 'Vice-Coordenador',
         },
