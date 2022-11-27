af4e87ee44214b2785345c4d12d26c1c3b454df4
mouracamila <camilapaleo@gmail.com>
2022-11-27 16:40:22 +0100

    change data to reminderAt in card reminder

--- src/modules/Classroom/components/CardReminder/index.tsx
+++ src/modules/Classroom/components/CardReminder/index.tsx
@@ -3,15 +3,15 @@ import { HiChevronRight } from 'react-icons/hi';
 
 export interface CardReminderProps {
   title: string;
-  data?: string;
+  reminderAt?: string;
 }
 
-const CardReminder: React.FC<CardReminderProps> = ({ title, data }) => {
+const CardReminder: React.FC<CardReminderProps> = ({ title, reminderAt }) => {
   return (
     <div className="flex items-center justify-between h-24 pl-6 pr-3 bg-white border-l-8 border-red-700 border-solid rounded-lg w-72">
       <div className="text-xl text-gray-600">
         <p>{title}</p>
-        <p className="font-light text-gray-600">Date:{data}</p>
+        <p className="font-light text-gray-600">Date:{reminderAt}</p>
       </div>
       <HiChevronRight className="w-11 h-11 fill-gray-500" />
     </div>
