938e861a2bf51fa9806884fcdbbf164ae2bf6d65
mouracamila <camilapaleo@gmail.com>
2022-12-02 16:20:23 +0100

    create mock calendar component

---
+++ src/modules/Classroom/components/Calendar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calendar: React.FC = () => {
+  return (
+    <div className="p-2 text-center bg-white rounded-lg h-72 w-72">
+      calendar
+    </div>
+  );
+};
+
+export default Calendar;
