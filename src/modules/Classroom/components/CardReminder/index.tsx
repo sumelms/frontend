8948e14124d267a5daf074c802a75dd04094ffe5
mouracamila <camilapaleo@gmail.com>
2022-12-02 16:21:08 +0100

    add hover to CardReminder

--- src/modules/Classroom/components/CardReminder/index.tsx
+++ src/modules/Classroom/components/CardReminder/index.tsx
@@ -8,13 +8,16 @@ export interface CardReminderProps {
 
 const CardReminder: React.FC<CardReminderProps> = ({ title, reminderAt }) => {
   return (
-    <div className="flex items-center justify-between h-24 pl-6 pr-3 bg-white border-l-8 border-red-700 border-solid rounded-lg w-72">
-      <div className="text-xl text-gray-600">
+    <a
+      href="/"
+      className="flex items-center justify-between h-24 pl-6 pr-3 bg-white border-l-8 border-red-500 border-solid rounded-lg hover:border-red-600 w-72 focus:ring-2 hover:bg-indigo-100/75 ring-indigo-500 ring-inset"
+    >
+      <div className="flex flex-col justify-center w-full h-full text-xl text-gray-700 justify-items-center hover:text-gray-900">
         <p>{title}</p>
-        <p className="font-light text-gray-600">Date:{reminderAt}</p>
+        <p className="font-light text-gray-700">Date:{reminderAt}</p>
       </div>
-      <HiChevronRight className="w-11 h-11 fill-gray-500" />
-    </div>
+      <HiChevronRight className="w-11 h-11 fill-gray-700 hover:fill-gray-900" />
+    </a>
   );
 };
 
