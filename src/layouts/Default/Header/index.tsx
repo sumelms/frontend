1b87ba7a0bda07d2f6019d7e427d75a6f7877f9a
Camila <41678283+mouracamila@users.noreply.github.com>
2023-03-05 16:31:45 +0100

    refactor: add new logo and action buttons, change colors (#178)

--- src/layouts/Default/Header/index.tsx
+++ src/layouts/Default/Header/index.tsx
@@ -1,9 +1,9 @@
 import { useKeycloak } from '@react-keycloak/web';
 import { Avatar, Button, DarkThemeToggle, Dropdown, Navbar } from 'flowbite-react';
 import React from 'react';
-import { HiLogout } from 'react-icons/hi';
+import { HiOutlineBell, HiOutlineLogout, HiOutlinePlay } from 'react-icons/hi';
 // eslint-disable-next-line import/no-unresolved
-import logoUrl from '~/assets/svg/sume-icon-dark.svg?url';
+import logoUrl from '../../../assets/svg/new-logo.svg';
 
 const Header: React.FC = () => {
   const { keycloak } = useKeycloak();
@@ -16,10 +16,16 @@ const Header: React.FC = () => {
 
   const ActionButtons = () => {
     return (
-      <div className="flex pl-4 ml-4 border-l">
+      <div className="flex pl-4 ml-4 border-l gap-2">
         <DarkThemeToggle />
-        <Button onClick={() => keycloak.logout()} className="!bg-transparent !text-gray-600">
-          <HiLogout className="w-4 h-4" />
+        <Button color="gray" className="border-none">
+          <HiOutlinePlay className="w-6 h-6 text-gray-600 dark:text-gray-400" />
+        </Button>
+        <Button color="gray" className="border-none">
+          <HiOutlineBell className="w-6 h-6 text-gray-600 dark:text-gray-400" />
+        </Button>
+        <Button onClick={() => keycloak.logout()} color="gray" className="border-none">
+          <HiOutlineLogout className="w-6 h-6 text-gray-600 dark:text-gray-400" />
         </Button>
       </div>
     );
@@ -29,8 +35,8 @@ const Header: React.FC = () => {
     return (
       <div className="flex items-center">
         <div className="flex flex-col mr-2 text-right">
-          <p className="text-sm font-semibold">{user.name}</p>
-          <span className="text-xs font-medium">{user.role}</span>
+          <p className="text-sm font-semibold text-gray-900 dark:text-gray-400">{user.name}</p>
+          <span className="text-xs font-medium text-gray-900 dark:text-gray-400">{user.role}</span>
         </div>
         <div>
           <Avatar alt={`${user.name}'s settings menu`} img={user.avatar} rounded={true} />
@@ -40,11 +46,11 @@ const Header: React.FC = () => {
   };
 
   return (
-    <Navbar fluid={true} rounded={false}>
+    <Navbar fluid={true} rounded={false} className="p-2">
       <Navbar.Brand href="https://sumelms.com/">
-        <img src={logoUrl} className="h-6 mr-3 sm:h-9" alt="Sume Logo" />
+        <img src={logoUrl} className="mr-3" alt="Sume Logo" />
       </Navbar.Brand>
-      <div className="flex bg-white md:order-2">
+      <div className="flex md:order-2 text-gray-900 dark:text-gray-400">
         <Dropdown arrowIcon={true} inline={true} label={<UserDetails />}>
           <Dropdown.Header>
             <span className="block text-sm">Bonnie Green</span>
