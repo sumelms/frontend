153cd35a5bcb8b892b9d034019e0f89282a0c983
Ricardo Lüders <rluders@redhat.com>
2023-03-07 18:07:08 +0100

    fix(theme): flowbite outline button hotfix

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,12 @@
-import type { CustomFlowbiteTheme } from "flowbite-react";
+import type { CustomFlowbiteTheme } from 'flowbite-react';
 
-const theme: CustomFlowbiteTheme = {};
+const theme: CustomFlowbiteTheme = {
+  button: {
+    outline: {
+      // hotfix - https://github.com/themesberg/flowbite-react/pull/654
+      on: 'bg-white text-gray-900 transition-all duration-75 ease-in group-hover:bg-opacity-0 group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full flex justify-center',
+    },
+  },
+};
 
 export default theme;
