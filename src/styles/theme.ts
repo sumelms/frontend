f358e33a044ba0d374d0240597e3f65d3e35ed5a
Ricardo Lüders <rluders@redhat.com>
2023-03-08 12:53:11 +0100

    chore: udpate flowbite-react

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,5 @@
 import type { CustomFlowbiteTheme } from 'flowbite-react';
 
-const theme: CustomFlowbiteTheme = {
-  button: {
-    outline: {
-      // hotfix - https://github.com/themesberg/flowbite-react/pull/654
-      on: 'bg-white text-gray-900 transition-all duration-75 ease-in group-hover:bg-opacity-0 group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full flex justify-center',
-    },
-  },
-};
+const theme: CustomFlowbiteTheme = {};
 
 export default theme;
