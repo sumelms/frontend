7a50ba3f496c2a24dda8831aa3196f9dba55e03c
Ricardo Lüders <rluders@redhat.com>
2023-10-12 16:32:40 +0200

    chore: update dependencies (#190)

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
-const keycloak = Keycloak({
+const keycloak = new Keycloak({
   url: import.meta.env.VITE_APP_KEYCLOAK_URL,
   realm: import.meta.env.VITE_APP_KEYCLOAK_REALM,
   clientId: import.meta.env.VITE_APP_KEYCLOAK_CLIENT_ID,
